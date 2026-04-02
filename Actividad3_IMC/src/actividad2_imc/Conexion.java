f88e4e889802df885906a8db58dc4670895f1727
daglezka <daglezka@gmail.com>
2026-04-02 16:33:44 -0700

    Add files via upload

---
+++ Actividad3_IMC/src/actividad2_imc/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad2_imc;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author Daniel Quezada
+ */
+public class Conexion {
+    Connection con = null;
+    String base = "";
+    String url = "";
+    String user = "";
+    String password = "";
+}
