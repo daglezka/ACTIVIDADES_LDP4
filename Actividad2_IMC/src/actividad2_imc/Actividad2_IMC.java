16230f673273f6dd865874b5b9ffbf7993eecae3
daglezka <daglezka@gmail.com>
2026-04-01 19:05:56 -0700

    Add files via upload

---
+++ Actividad2_IMC/src/actividad2_imc/Actividad2_IMC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad2_imc;
+
+/**
+ *
+ * @author Daniel Quezada
+ */
+public class Actividad2_IMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
